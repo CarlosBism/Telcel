fd3e94e8a1b29db1f5c010d38f61a5f3a549e11b
CarlosBism <carlos.banuelos.bism.com.mx>
2023-12-13 16:33:37 -0600

    Creando conflicto

--- src/main/java/com/Tecel/Dao/TelefonosDao.java
+++ src/main/java/com/Tecel/Dao/TelefonosDao.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TelefonosDao extends JpaRepository<Telefonos, Long> {
     //Si quieres agregar otro tipo de busqueds se coloca aca
+    //Generando un conflicto
+    //Crando un conflicto
 }
